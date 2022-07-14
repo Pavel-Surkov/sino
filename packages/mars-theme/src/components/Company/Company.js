97d17582bccf14869fb0d4e64c0c7ad0ab700f46
Pavel Surkov <surkovv.pavel@gmail.com>
2022-07-14 14:15:01 +0700

    adds vision and mission section

--- packages/mars-theme/src/components/Company/Company.js
+++ packages/mars-theme/src/components/Company/Company.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../constant/HeroSection";
 import History from "./History/History";
 import Timeline from "./Timeline/Timeline";
+import VisionAndMission from "./VisionAndMission/VisionAndMission";
 import { connect, styled } from "frontity";
 
 import heroImg from "../../assets/images/company-about-hero.png";
@@ -12,6 +13,7 @@ const Company = ({ state }) => {
       <Hero title="Our Company" image={heroImg} />
       <History />
       <Timeline />
+      <VisionAndMission />
     </CompanyWrapper>
   );
 };

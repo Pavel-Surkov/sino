65ef813355bc18c87f90f6ee0998c4590b92c8f3
Pavel Surkov <surkovv.pavel@gmail.com>
2022-07-29 20:23:36 +0700

    adds styling to the main content in job-details page

--- packages/mars-theme/src/components/JobDetails/Info/Info.js
+++ packages/mars-theme/src/components/JobDetails/Info/Info.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Container from "../../constant/Container";
 import Position from "./Position/Position";
+import TextBlock from "../../PrivacyPolicy/TextBlock/TextBlock";
+import PrimaryBtn from "../../constant/PrimaryButton";
+import { font } from "../../base/functions";
 import { styled } from "frontity";
 
 import company from "../../../assets/images/company-image.jpg";
 import company2x from "../../../assets/images/company-image@2x.jpg";
+
+const responsibilities = [
+  "To handle Export-Import shipment.",
+  "Prepare Import-Export declaration.",
+  "Check accuracy of documents.",
+  "Provide other reports to CS team.",
+  "Service Delivery (Both Quality and Timeliness).",
+  "Have knowledge of the import and export process.",
+];
+
+const requirements = [
+  "Male / Female age 22-30 years.",
+  "Good command on computer skills.",
+  "Good Microsoft office, Excel, and Word.",
+  "Good command on writes, read English.",
+  "Ability to work as a team.",
+  "Ability to work under pressure.",
+  "Good cooperation and coordination with others on duty and work as teamwork.",
+  "Ability to deliver urgently & completed the job on time as per schedule and commitment.",
+  "Required experience and qualifications: Minimum 1-year experience in Customs data entry/Customs Clearance/Certificate of origin/License/Permit is an advantage.",
+];
 
 const Info = () => {
   return (
@@ -12,7 +36,33 @@ const Info = () => {
       <Container>
         <Position />
         <InfoBlock>
-          <Content></Content>
+          <Content>
+            <TextBlock marginBottom={28} title="Key responsibilities">
+              <List>
+                {responsibilities.map((item) => (
+                  <ListItem key={item.slice(0, 11)}>{item}</ListItem>
+                ))}
+              </List>
+            </TextBlock>
+            <TextBlock marginBottom={28} title="We are looking for">
+              <List>
+                {requirements.map((item) => (
+                  <ListItem key={item.slice(0, 11)}>{item}</ListItem>
+                ))}
+              </List>
+            </TextBlock>
+            <TextBlock marginBottom={28} title="To apply">
+              <Text>
+                <p>
+                  Please send your resume indicating experience and expected
+                  salary using the button below.
+                </p>
+              </Text>
+              <ButtonWrapper>
+                <PrimaryBtn content="Apply Now" />
+              </ButtonWrapper>
+            </TextBlock>
+          </Content>
           <ImageWrapper>
             <img
               src={company}
@@ -26,9 +76,76 @@ const Info = () => {
   );
 };
 
-const Content = styled.div``;
+const ButtonWrapper = styled.div`
+  padding-top: 16px;
+`;
 
-const ImageWrapper = styled.div``;
+const Text = styled.div`
+  & p {
+    margin: 0;
+    ${font(18, 30)};
+    font-weight: 300;
+    margin-bottom: 8px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  padding-left: 16px;
+  margin-bottom: 8px;
+  ${font(18, 30)};
+  font-weight: 300;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  &::before {
+    position: absolute;
+    content: "";
+    left: 0;
+    top: 10px;
+    width: 4px;
+    height: 4px;
+    border: 2px solid var(--black);
+    border-radius: 50%;
+  }
+`;
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  padding-left: 24px;
+  @media screen and (max-width: 991px) {
+    padding: 0;
+  }
+`;
+
+const Content = styled.div`
+  max-width: 558px;
+  & div[data-element="wrapper"] {
+    margin-bottom: 48px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  @media screen and (max-width: 991px) {
+    & h5 {
+      font-weight: 400;
+    }
+  }
+`;
+
+const ImageWrapper = styled.div`
+  & img {
+    max-width: 100%;
+  }
+  @media screen and (max-width: 991px) {
+    display: none;
+  }
+`;
 
 const InfoBlock = styled.div`
   padding-top: 96px;
@@ -41,6 +158,8 @@ const InfoBlock = styled.div`
     padding-top: 56px;
     padding-bottom: 0;
     border: none;
+    grid-template-columns: 100%;
+    grid-gap: 0;
   }
 `;
 
